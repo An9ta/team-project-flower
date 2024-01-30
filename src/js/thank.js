db78ba24528d0dcc3bfdfc2388610510d962c470
Remi Kujawski <remi.kujawski@gmail.com>
2024-01-30 19:44:22 +0100

    Thank You update

--- src/js/thank.js
+++ src/js/thank.js
@@ -2,12 +2,12 @@
 
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-thank-modal-open]'),
+    // openModalBtn: document.querySelector('[data-thank-modal-open]'),
     closeModalBtn: document.querySelector('[data-thank-modal-close]'),
     modal: document.querySelector('[data-thank-data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
+  // refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
